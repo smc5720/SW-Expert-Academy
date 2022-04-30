ead317cba0b850e690d7c8bb678b1835d2d3d0bb
smc5720 <smc5720@gmail.com>
2022-04-30 18:09:29 +0900

    20220430

--- 핀볼 게임.java
+++ 핀볼 게임.java
@@ -1,69 +1,138 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+import java.io.FileInputStream;
 
 class Solution {
-	public static int[][] map, timer, tmp;
-	public static int N, M, K;
+	public static int N, by, bx, bd, score, maxScore;
+	public static Block[][] block;
 	public static int[] dy = { 0, 0, -1, 1 };
 	public static int[] dx = { -1, 1, 0, 0 };
-	public static Cell[][] cell;
-	public static Queue<Cell> queue, waitQueue;
+	public static final int LEFT = 0;
+	public static final int RIGHT = 1;
+	public static final int UP = 2;
+	public static final int DOWN = 3;
 
-	public static enum CellType {
-		none, wait, inact, act, die
-	};
+	public static class Block {
+		int y, x, type;
 
-	public static class Cell {
-		int y, x, val, timer;
-		CellType state;
-
-		public Cell(int y, int x, int val, int timer, CellType state) {
+		public Block(int y, int x, int type) {
 			this.y = y;
 			this.x = x;
-			this.val = val;
-			this.timer = timer;
-			this.state = state;
+			this.type = type;
 		}
 
-		public void spend() {
-			if (state == CellType.die || state == CellType.none || state == CellType.wait) {
-				return;
+		// 공의 방향을 변경한다.
+		public void receive() {
+			if (1 <= type && type <= 5) {
+				score += 1;
+
+				// 공의 방향이 왼쪽이라면
+				if (bd == LEFT) {
+					if (type == 3 || type == 4 || type == 5) {
+						bd = RIGHT;
+					}
+
+					if (type == 1) {
+						bd = UP;
+					}
+
+					if (type == 2) {
+						bd = DOWN;
+					}
+
+					return;
+				}
+
+				// 공의 방향이 오른쪽이라면
+				if (bd == RIGHT) {
+					if (type == 1 || type == 2 || type == 5) {
+						bd = LEFT;
+					}
+
+					if (type == 3) {
+						bd = DOWN;
+					}
+
+					if (type == 4) {
+						bd = UP;
+					}
+
+					return;
+				}
+
+				// 공의 방향이 위쪽이라면
+				if (bd == UP) {
+					if (type == 1 || type == 4 || type == 5) {
+						bd = DOWN;
+					}
+
+					if (type == 2) {
+						bd = RIGHT;
+					}
+
+					if (type == 3) {
+						bd = LEFT;
+					}
+
+					return;
+				}
+
+				// 공의 방향이 아래쪽이라면
+				if (bd == DOWN) {
+					if (type == 2 || type == 3 || type == 5) {
+						bd = UP;
+					}
+
+					if (type == 1) {
+						bd = RIGHT;
+					}
+
+					if (type == 4) {
+						bd = LEFT;
+					}
+
+					return;
+				}
 			}
 
-			timer -= 1;
-
-			if (state == CellType.act && timer == val - 1) {
-				spread();
-			}
-
-			if (timer == 0) {
-				if (state == CellType.inact) {
-					timer = val;
-					state = CellType.act;
-				} else if (state == CellType.act) {
-					state = CellType.die;
+			if (6 <= type && type <= 10) {
+				for (int i = 0; i < N; i++) {
+					for (int j = 0; j < N; j++) {
+						if (block[i][j].type == type && (i != y || j != x)) {
+							by = i;
+							bx = j;
+						}
+					}
 				}
 			}
 		}
 
-		public void spread() {
-			for (int i = 0; i < 4; i++) {
-				int ty = y + dy[i];
-				int tx = x + dx[i];
+		// 공의 좌표를 이동시킨다.
+		public void send() {
+			by += dy[bd];
+			bx += dx[bd];
 
-				if (cell[ty][tx].state == CellType.none
-						|| (cell[ty][tx].state == CellType.wait && cell[ty][tx].val < val)) {
-					cell[ty][tx].state = CellType.wait;
-					cell[ty][tx].val = val;
-					waitQueue.add(cell[ty][tx]);
+			if (!(0 <= by && by < N && 0 <= bx && bx < N)) {
+				score += 1;
+
+				if (bd == UP) {
+					bd = DOWN;
 				}
-			}
-		}
 
-		public void awake() {
-			state = CellType.inact;
-			timer = val;
-			queue.add(this);
+				else if (bd == DOWN) {
+					bd = UP;
+				}
+
+				else if (bd == LEFT) {
+					bd = RIGHT;
+				}
+
+				else if (bd == RIGHT) {
+					bd = LEFT;
+				}
+
+				by += dy[bd];
+				bx += dx[bd];
+			}
 		}
 	}
 
@@ -74,109 +143,42 @@ class Solution {
 
 		for (int test_case = 1; test_case <= T; test_case++) {
 			N = sc.nextInt();
-			M = sc.nextInt();
-			K = sc.nextInt();
+			block = new Block[N][N];
+			maxScore = 0;
 
-			cell = new Cell[N + K * 2][M + K * 2];
-			queue = new LinkedList<Cell>();
-			waitQueue = new LinkedList<Cell>();
-
-			for (int y = 0; y < N + K * 2; y++) {
-				for (int x = 0; x < M + K * 2; x++) {
-					cell[y][x] = new Cell(y, x, 0, 0, CellType.none);
+			for (int y = 0; y < N; y++) {
+				for (int x = 0; x < N; x++) {
+					block[y][x] = new Block(y, x, sc.nextInt());
 				}
 			}
 
 			for (int y = 0; y < N; y++) {
-				for (int x = 0; x < M; x++) {
-					int n = sc.nextInt();
+				for (int x = 0; x < N; x++) {
+					for (int i = 0; i < 4; i++) {
+						score = 0;
 
-					if (n != 0) {
-						cell[y + K][x + K] = new Cell(y + K, x + K, n, n, CellType.inact);
-						queue.add(cell[y + K][x + K]);
+						if (block[y][x].type != 0) {
+							continue;
+						}
+
+						by = y;
+						bx = x;
+						bd = i;
+
+						int sy = by;
+						int sx = bx;
+
+						do {
+							block[by][bx].send();
+							block[by][bx].receive();
+						} while (!((sy == by && sx == bx) || block[by][bx].type == -1));
+
+						maxScore = Math.max(maxScore, score);
 					}
 				}
 			}
 
-			// printMap();
-
-			for (int k = 0; k < K; k++) {
-				awakeCell();
-				spendTime();
-				// printMap();
-			}
-
-			System.out.printf("#%d %d\n", test_case, count());
+			System.out.printf("#%d %d\n", test_case, maxScore);
 		}
-	}
-
-	public static void spendTime() {
-		int size = queue.size();
-
-		for (int i = 0; i < size; i++) {
-			Cell c = queue.poll();
-			c.spend();
-
-			if (c.state != CellType.die) {
-				queue.add(c);
-			}
-		}
-	}
-
-	public static void awakeCell() {
-		while (!waitQueue.isEmpty()) {
-			waitQueue.poll().awake();
-		}
-	}
-
-	public static void printMap() {
-		System.out.println();
-
-		for (int y = 0; y < N + K * 2; y++) {
-			for (int x = 0; x < M + K * 2; x++) {
-				if (cell[y][x].state == CellType.none) {
-					System.out.printf("- ");
-				} else if (cell[y][x].state == CellType.wait) {
-					System.out.printf("W ");
-				} else if (cell[y][x].state == CellType.die) {
-					System.out.printf("■ ");
-				} else {
-					System.out.printf("%d ", cell[y][x].val);
-				}
-			}
-
-			System.out.printf("\t\t");
-
-			for (int x = 0; x < M + K * 2; x++) {
-				if (cell[y][x].state == CellType.none) {
-					System.out.printf("- ");
-				} else if (cell[y][x].state == CellType.wait) {
-					System.out.printf("W ");
-				} else if (cell[y][x].state == CellType.die) {
-					System.out.printf("■ ");
-				} else {
-					System.out.printf("%d ", cell[y][x].timer);
-				}
-			}
-
-			System.out.println();
-		}
-
-		System.out.println();
-	}
-
-	public static int count() {
-		int cnt = 0;
-
-		for (int y = 0; y < N + K * 2; y++) {
-			for (int x = 0; x < M + K * 2; x++) {
-				if (cell[y][x].state == CellType.wait || cell[y][x].state == CellType.inact
-						|| cell[y][x].state == CellType.act) {
-					cnt += 1;
-				}
-			}
-		}
-
-		return cnt;
 	}
 }
